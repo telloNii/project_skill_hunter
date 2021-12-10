7fde3c7c60e5ec259d9bd2c0ec63df7ede590d33
Todd Nelson <tello_nii@outlook.com>
2021-12-10 18:49:40 +0000

    refactored code

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,228 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  static final String id = "sign up screen route";
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  bool obscureText = true;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  late String? userEmail;
+  late String? userPassword;
+  late String? userName;
+  final _usernameTextControler = TextEditingController();
+  final _emailTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.longestSide * 0.9,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 12.0),
+                    child: Text(
+                      "Register",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 0.0, horizontal: 12.0),
+                    child: Text(
+                      "First let's get you registered",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey.shade600),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 50,
+                      child: TextFormField(
+                        controller: _usernameTextControler,
+                        onChanged: (String username) {
+                          setState(() {
+                            userName = username;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Please enter your Username",
+                          labelText: "UserName",
+                          labelStyle: TextStyle(color: Colors.lightBlue),
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.black)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 50,
+                      child: TextFormField(
+                        controller: _emailTextController,
+                        onChanged: (String email) {
+                          setState(() {
+                            userEmail = email;
+                          });
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          hintText: "Please enter your Email",
+                          labelText: "Email",
+                          labelStyle: TextStyle(color: Colors.lightBlue),
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(color: Colors.black)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 50,
+                      child: Visibility(
+                        child: TextFormField(
+                          controller: _passwordTextController,
+                          onChanged: (String password) {
+                            setState(() {
+                              userPassword = password;
+                            });
+                          },
+                          obscureText: obscureText,
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: InputDecoration(
+                            hintText: "Please enter your Password",
+                            labelText: "Password",
+                            labelStyle: TextStyle(color: Colors.lightBlue),
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                if (obscureText == true) {
+                                  setState(() {
+                                    obscureText = false;
+                                  });
+                                } else {
+                                  setState(() {
+                                    obscureText = true;
+                                  });
+                                }
+                              },
+                              icon: Icon(Icons.visibility),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide(color: Colors.black)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Text(
+                      "An email confirmation link will be sent to your email address.",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 40,
+                      width: MediaQuery.of(context).size.width,
+                      child: OutlinedButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.black),
+                          ),
+                          onPressed: () async {
+                            if (userName != null &&
+                                userEmail!.contains("@") &&
+                                userPassword != null) {
+                              await _auth
+                                  .createUserWithEmailAndPassword(
+                                      email: userEmail!,
+                                      password: userPassword!)
+                                  .then(
+                                (value) {
+                                  final user = _auth.currentUser;
+                                  return user!.updateDisplayName(userName);
+                                },
+                              );
+                              Navigator.pop(context);
+                            }
+                          },
+                          child: Text(
+                            "Register",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20, horizontal: 20.0),
+              child: Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  Text(
+                    "By registering, you agree to Skill Hunter's ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  GestureDetector(
+                    child: Text(
+                      "Terms of Service ",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.green,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/fetch_home_screen _data.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:project_skill_hunter/widgets/home_screen_widget.dart';
+
+final _firestore = FirebaseFirestore.instance;
+
+class FetchDatabaseData extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: _firestore.collection('Menu').snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          List<Widget> menuItemWidgets = [];
+          // displaying a circular progress indicator when fetch data or when no data is available
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.blue.shade900,
+              ),
+            );
+          } else {
+            final menuItems = snapshot.data!.docs.reversed;
+
+            for (var menuItem in menuItems) {
+              final userImage = menuItem.data()["Image"];
+              final userName = menuItem.data()['UserName'];
+              final email = menuItem.data()['Email'];
+              final fullName = menuItem.data()['Full name'];
+              final userID = menuItem.data()['UID'];
+              final location = menuItem.data()['Location'];
+              final dateOfBirth = menuItem.data()['Date Of Birth'];
+
+              final menuItemWidget = HomeScreenWidget(
+                  image: userImage, fullName: fullName, email: email);
+              menuItemWidgets.add(menuItemWidget);
+
+              // print(menuItemPrice);
+            }
+          }
+          return Container(
+              height: MediaQuery.of(context).size.longestSide,
+              child: ListView(reverse: false, children: menuItemWidgets));
+        });
+  }
+}
