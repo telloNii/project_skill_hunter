15da2fb6b1e6a0608cf3becb2eeda4bb9056c0e7
Todd Nelson <tello_nii@outlook.com>
2021-12-10 18:50:33 +0000

    refactored code

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:project_skill_hunter/screens/profile_screen.dart';
+import 'package:project_skill_hunter/screens/search_screen.dart';
+import 'package:project_skill_hunter/services/fetch_home_screen%20_data.dart';
+
+class HomeScreen extends StatefulWidget {
+  static final String id = "home screen route";
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int bottomNavBarIndex = 0;
+  void newIndex(int index) {
+    setState(() {
+      bottomNavBarIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List displayScreeen = [
+      FetchDatabaseData(),
+      SearchScreen(),
+      ProfileScreen()
+    ];
+    List displayAppBar = [
+      AppBar(
+        elevation: 2,
+        backgroundColor: Colors.brown,
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text(
+          "Skill Hunter",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      null,
+      null
+    ];
+    return Scaffold(
+      appBar: displayAppBar[bottomNavBarIndex],
+      body: displayScreeen[bottomNavBarIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: newIndex,
+        currentIndex: bottomNavBarIndex,
+        selectedItemColor: Colors.brown,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.search_sharp), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.person_sharp), label: ""),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:project_skill_hunter/screens/signin_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  static final String id = "splash screen router";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.shortestSide,
+        height: MediaQuery.of(context).size.longestSide,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/background_image.jpg"),
+              fit: BoxFit.fill),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.shortestSide,
+                height: 35,
+                child: OutlinedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.cyan)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, SignInScreen.id);
+                  },
+                  child: Text(
+                    "Sign Up/Sign In",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
