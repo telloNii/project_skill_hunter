c5a34f5e2a36f1ac2b18c55eece3c75b2e891879
Todd Nelson <tello_nii@outlook.com>
2021-12-13 02:53:36 +0000

    new funtional screen

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  static final String id = "settings screen route";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+        centerTitle: true,
+        title: Text("Settings"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.person_rounded),
+                title: Text("Update profile"),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.notifications),
+                title: Text("Notifications"),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.security_rounded),
+                title: Text("Security"),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
