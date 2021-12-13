7e541c3805908941f58ec92dbd5e1ace314801cf
Todd Nelson <tello_nii@outlook.com>
2021-12-13 02:06:18 +0000

    new stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_skill_hunter/screens/home_screen.dart';
+import 'package:project_skill_hunter/screens/onboarding_screen_wizard.dart';
 import 'package:project_skill_hunter/screens/signin_screen.dart';
 import 'package:project_skill_hunter/screens/signup_screen.dart';
 import 'package:project_skill_hunter/screens/splash_screen.dart';
@@ -15,11 +16,13 @@ class SkillHunter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         SplashScreen.id: (context) => SplashScreen(),
         SignInScreen.id: (context) => SignInScreen(),
         SignUpScreen.id: (context) => SignUpScreen(),
         HomeScreen.id: (context) => HomeScreen(),
+        OnBoardWizard.id: (context) => OnBoardWizard(),
       },
       initialRoute: SplashScreen.id,
     );
--- lib/screens/onboarding_screen_wizard.dart
+++ lib/screens/onboarding_screen_wizard.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:project_skill_hunter/screens/home_screen.dart';
 
 class OnBoardWizard extends StatefulWidget {
+  static final String id = "onboarding wizard route";
   @override
   _OnBoardWizardState createState() => _OnBoardWizardState();
 }
@@ -12,121 +15,55 @@ class _OnBoardWizardState extends State<OnBoardWizard> {
   FirebaseAuth _auth = FirebaseAuth.instance;
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  final _usernameTextControler = TextEditingController();
-  final _emailTextController = TextEditingController();
-  final _passwordTextController = TextEditingController();
+  final _fullnameTextControler = TextEditingController();
+  final _dateOfBirthTextController = TextEditingController();
+  final _locationTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.longestSide * 0.9,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 12.0),
-                      child: Text(
-                        "Register",
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 0.0, horizontal: 12.0),
-                      child: Text(
-                        "First let's get you registered",
-                        style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.grey.shade600),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        height: 50,
-                        child: TextFormField(
-                          controller: _usernameTextControler,
-                          decoration: InputDecoration(
-                            hintText: "Please enter your Username",
-                            labelText: "UserName",
-                            labelStyle: TextStyle(color: Colors.lightBlue),
-                            floatingLabelBehavior: FloatingLabelBehavior.always,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(color: Colors.black)),
-                          ),
+      body: SafeArea(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ListView(
+              children: [
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 12.0),
+                        child: Text(
+                          "Basic Infomation",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.w500),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        height: 50,
-                        child: TextFormField(
-                          controller: _emailTextController,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            hintText: "Please enter your Email",
-                            labelText: "Email",
-                            labelStyle: TextStyle(color: Colors.lightBlue),
-                            floatingLabelBehavior: FloatingLabelBehavior.always,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(color: Colors.black)),
-                          ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 0.0, horizontal: 12.0),
+                        child: Text(
+                          "Let's complete get you registered",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.grey.shade600),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        height: 50,
-                        child: Visibility(
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          height: 50,
                           child: TextFormField(
-                            controller: _passwordTextController,
-                            obscureText: obscureText,
-                            keyboardType: TextInputType.visiblePassword,
+                            controller: _fullnameTextControler,
                             decoration: InputDecoration(
-                              hintText: "Please enter your Password",
-                              labelText: "Password",
+                              hintText: "Please enter your Username",
+                              labelText: "Full Name",
                               labelStyle: TextStyle(color: Colors.lightBlue),
                               floatingLabelBehavior:
                                   FloatingLabelBehavior.always,
-                              suffixIcon: IconButton(
-                                onPressed: () {
-                                  if (obscureText == true) {
-                                    setState(() {
-                                      obscureText = false;
-                                    });
-                                  } else {
-                                    setState(() {
-                                      obscureText = true;
-                                    });
-                                  }
-                                },
-                                icon: Icon(Icons.visibility),
-                              ),
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(10),
                               ),
@@ -137,98 +74,148 @@ class _OnBoardWizardState extends State<OnBoardWizard> {
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                      child: Text(
-                        "An email confirmation link will be sent to your email address.",
-                        textAlign: TextAlign.start,
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          height: 50,
+                          child: TextFormField(
+                            onTap: () async {
+                              FocusScope.of(context)
+                                  .requestFocus(new FocusNode());
+                              var date = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(1900),
+                                  lastDate: DateTime.now());
+                              _dateOfBirthTextController.text =
+                                  date!.toString().substring(0, 10);
+                              print(_dateOfBirthTextController.text);
+                            },
+                            controller: _dateOfBirthTextController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              hintText: "Please enter your Date of Birth",
+                              labelText: "Date Of Birth",
+                              labelStyle: TextStyle(color: Colors.lightBlue),
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.always,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(color: Colors.black)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          height: 50,
+                          child: Visibility(
+                            child: TextFormField(
+                              controller: _locationTextController,
+                              keyboardType: TextInputType.visiblePassword,
+                              decoration: InputDecoration(
+                                hintText: "Please enter your Location",
+                                labelText: "Location",
+                                labelStyle: TextStyle(color: Colors.lightBlue),
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide:
+                                        BorderSide(color: Colors.black)),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Text(
+                          "An email confirmation link will be sent to your email address.",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          height: 40,
+                          width: MediaQuery.of(context).size.width,
+                          child: OutlinedButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.black),
+                              ),
+                              onPressed: () async {
+                                if (_fullnameTextControler != null &&
+                                    _dateOfBirthTextController != null &&
+                                    _locationTextController != null) {
+                                  final user = _auth.currentUser;
+                                  CollectionReference ref = FirebaseFirestore
+                                      .instance
+                                      .collection('users');
+
+                                  String docId = ref.doc().id;
+                                  setState(() {
+                                    docId = _auth.currentUser!.uid.toString();
+                                    user!.updateDisplayName(
+                                        _fullnameTextControler.toString());
+                                  });
+
+                                  await ref.doc(docId).set({
+                                    'fullName': _fullnameTextControler.text,
+                                    'date of birth':
+                                        _dateOfBirthTextController.text,
+                                    'Location': _locationTextController.text,
+                                  }, SetOptions(merge: true));
+                                  print(_auth.currentUser!.uid);
+                                  print(docId);
+                                  Navigator.popAndPushNamed(
+                                      context, HomeScreen.id);
+                                }
+                              },
+                              child: Text(
+                                "Register",
+                                style: TextStyle(color: Colors.white),
+                              )),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10, horizontal: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "By registering, you agree to Skill Hunter's ",
+                        textAlign: TextAlign.center,
                         style: TextStyle(fontSize: 12),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        height: 40,
-                        width: MediaQuery.of(context).size.width,
-                        child: OutlinedButton(
-                            style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.black),
-                            ),
-                            onPressed: () async {
-                              if (_usernameTextControler != null &&
-                                  _emailTextController
-                                      .toString()
-                                      .contains("@") &&
-                                  _passwordTextController != null) {
-                                await _auth
-                                    .createUserWithEmailAndPassword(
-                                        email: _emailTextController.toString(),
-                                        password:
-                                            _passwordTextController.toString())
-                                    .then(
-                                  (value) {
-                                    final user = _auth.currentUser;
-
-                                    return user!.updateDisplayName(
-                                        _usernameTextControler.toString());
-                                  },
-                                );
-                                CollectionReference ref =
-                                    await FirebaseFirestore.instance
-                                        .collection('users');
-
-                                String docId = ref.doc().id;
-                                setState(() {
-                                  docId = _auth.currentUser!.uid.toString();
-                                });
-
-                                await ref
-                                    .doc(docId)
-                                    .set({'id': ref.doc().id}).then((value) {
-                                  // Do something
-                                }).onError((error, stackTrace) {
-                                  print(stackTrace);
-                                });
-
-                                Navigator.pop(context);
-                              }
-                            },
-                            child: Text(
-                              "Register",
-                              style: TextStyle(color: Colors.white),
-                            )),
+                      GestureDetector(
+                        child: Text(
+                          "Terms of Service ",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.green,
+                              fontWeight: FontWeight.w600),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 20, horizontal: 20.0),
-                child: Wrap(
-                  alignment: WrapAlignment.center,
-                  children: [
-                    Text(
-                      "By registering, you agree to Skill Hunter's ",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    GestureDetector(
-                      child: Text(
-                        "Terms of Service ",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.green,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
