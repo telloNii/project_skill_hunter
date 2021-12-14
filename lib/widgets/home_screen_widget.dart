c6ac4fd00b4ac9dd6bff8344a8acd75db209392b
Todd Nelson <tello_nii@outlook.com>
2021-12-14 01:39:54 +0000

    rebuilt widget

--- lib/widgets/home_screen_widget.dart
+++ lib/widgets/home_screen_widget.dart
@@ -1,46 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class HomeScreenWidget extends StatelessWidget {
-  HomeScreenWidget(
-      {required this.image, required this.fullName, required this.email});
-  late final String image, fullName, email;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 3.0, horizontal: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-          boxShadow: [BoxShadow(color: Color(0xffaaccbb))],
-          border: Border.all(width: 0.2),
+  HomeScreenWidget({
+    required final String image,
+    required final String fullName,
+    required final String email,
+  })  : _image = image,
+        _fullName = fullName,
+        _email = email;
+  late final String _image, _fullName, _email;
+  late final bool _isMe;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  Widget returnCard() {
+    if (_email == _auth.currentUser!.email) {
+      _isMe = true;
+    }
+    return Visibility(
+      visible: _isMe ? false : true,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 3.0,
         ),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CircleAvatar(
-                radius: 30,
-                backgroundImage: NetworkImage(image),
+        child: Card(
+          elevation: 5,
+          shadowColor: Color(0x55ffffff),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(_image),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    fullName,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text(email),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      _fullName,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(_email),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return returnCard();
   }
 }
