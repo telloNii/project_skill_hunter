7050524d5a4901b77a30a2f0f99bd38edf4ec995
Todd Nelson <tello_nii@outlook.com>
2021-12-17 03:55:24 +0000

    refactored code and added navigation to new screen

--- lib/widgets/home_screen_widget.dart
+++ lib/widgets/home_screen_widget.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:project_skill_hunter/screens/view_card_screen.dart';
 
 // ignore: must_be_immutable
 class HomeScreenWidget extends StatelessWidget {
@@ -12,52 +12,55 @@ class HomeScreenWidget extends StatelessWidget {
         _fullName = fullName,
         _email = email;
   late final String _image, _fullName, _email;
-  late final bool _isMe;
+  bool _isMe = false;
   FirebaseAuth _auth = FirebaseAuth.instance;
-  Widget returnCard() {
+
+  @override
+  Widget build(BuildContext context) {
     if (_email == _auth.currentUser!.email) {
       _isMe = true;
     }
+
     return Visibility(
       visible: _isMe ? false : true,
       child: Padding(
         padding: const EdgeInsets.symmetric(
           vertical: 3.0,
         ),
-        child: Card(
-          elevation: 5,
-          shadowColor: Color(0x55ffffff),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CircleAvatar(
-                  radius: 30,
-                  backgroundImage: NetworkImage(_image),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, ViewCardScreen.id);
+          },
+          child: Card(
+            elevation: 5,
+            shadowColor: Color(0x55ffffff),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(_image),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      _fullName,
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Text(_email),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        _fullName,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(_email),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return returnCard();
   }
 }
