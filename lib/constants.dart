3742ba5e5333c38c40cb6fd8ebe1fb838d3ddb09
Todd Nelson <tello_nii@outlook.com>
2021-12-25 22:37:36 +0000

    added new font

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 InputDecoration textFieldInputDecoration = InputDecoration(
-  hintStyle: TextStyle(color: Colors.white),
+  hintStyle: TextStyle(color: Colors.white, fontFamily: "Oxygen"),
   hintText: "Please enter your ",
   fillColor: Colors.grey.withOpacity(0.4),
   filled: true,
@@ -9,13 +9,23 @@ InputDecoration textFieldInputDecoration = InputDecoration(
     borderRadius: BorderRadius.circular(10),
   ),
   focusedBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(10),
-      borderSide: BorderSide(color: Colors.black)),
+    borderRadius: BorderRadius.circular(10),
+    borderSide: BorderSide(color: Colors.black),
+  ),
 );
 
-TextStyle authPageHeaderTextStyle =
-    TextStyle(fontSize: 24, fontWeight: FontWeight.w500, color: Colors.white);
+TextStyle authPageHeaderTextStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+  fontFamily: "ProximaNova",
+);
 
-const TextStyle textFieldLabel = TextStyle(color: Color(0xFFA18975), fontSize: 16);
-TextStyle unselectedAuthPageHeaderTextStyle =
-    authPageHeaderTextStyle.copyWith(fontWeight: FontWeight.w300);
+const TextStyle textFieldLabel = TextStyle(
+  color: Color(0xFFA18975),
+  fontSize: 16,
+  fontFamily: "ProximaNova",
+);
+TextStyle unselectedAuthPageHeaderTextStyle = authPageHeaderTextStyle.copyWith(
+  fontWeight: FontWeight.w300,
+);
